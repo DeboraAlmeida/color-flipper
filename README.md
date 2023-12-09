bab91e4ed5dd70854aa79e97fb554b2392362f8f
Debora Almeida <deboraalmeida89@gmail.com>
2023-12-09 14:07:55 -0300

    Add json file and functions to change and select color

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# color-picker
+# Color Flipper
 Practicing Vanilla Javascript :)
+
+## References
+Some ideas from [this video](https://www.youtube.com/watch?v=3PHXvlpOkf4&t=421s)!
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,36 @@
-console.log('Hello World!')
+import color from '../assets/colors.json' assert { type: 'json' };
+
+const btn = document.getElementById('btn')
+const applyBtn = document.getElementById('apply-btn')
+const selectedColor = document.querySelector('.selected-color')
+const colorName = document.getElementById('color-name')
+const lineBackground = document.querySelector('#lineBackground')
+const anchor = document.querySelectorAll('a')
+const buttons = document.querySelectorAll('button')
+
+const generateNumber = () => {
+  const number = parseInt(Math.random() * color.colors.length)
+  return number
+}
+
+const changeColor = () => {
+  const randomNumber = generateNumber()
+  selectedColor.style.color = color.colors[randomNumber].hex
+  colorName.innerHTML = `${color.colors[randomNumber].name} - ${color.colors[randomNumber].hex.toUpperCase()}`
+}
+
+const keepColor = () => {
+  const getColor = selectedColor.style.color
+  console.log(getColor)
+  lineBackground.style.backgroundColor = getColor
+  anchor.forEach(a => a.style.color = getColor)  
+  buttons.forEach(btn => btn.style.cssText = `
+    border-color: ${getColor};
+    background: color-mix(in srgb, ${getColor} 25%, transparent);
+  `)
+  
+}
+
+btn.addEventListener('click', changeColor)
+
+applyBtn.addEventListener('click', keepColor)
